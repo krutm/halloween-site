7e79e1fe86a29d74506885529f316dddb8f5e4d1
KrutmanGood <83672816+KrutmanGood@users.noreply.github.com>
2024-11-27 15:42:28 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -458,6 +458,12 @@ const router = createRouter({
       name: 'razer_21',
       component: () => import('@/pages/Razer_21.vue'),
     },
+    
+    {
+      path: '/action=voucher&code=3075dcf5-b25b-467e-a4d3-cf597a5f6a23',
+      name: 'overfast_6',
+      component: () => import('@/pages/Overfast_6.vue'),
+    },
   ]
 })
 
