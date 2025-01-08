aacf8582f08d18bdecf12b94b4b06643e073209b
krutm <83672816+krutm@users.noreply.github.com>
2025-01-08 14:49:26 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -548,6 +548,12 @@ const router = createRouter({
       name: 'Overfast_11',
       component: () => import('@/pages/Overfast_11.vue'),
     },
+    
+    {
+      path: '/action=voucher&code=3089dcf5-b25b-467e-a4d3-cf597a5f6a23',
+      name: 'Sirex_111',
+      component: () => import('@/pages/Lemme_3.vue'),
+    },
   ]
 })
 
