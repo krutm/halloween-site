acf4bdb26089cbc1aab4f7637b3bcee2844cce67
krutm <83672816+krutm@users.noreply.github.com>
2025-01-17 19:15:53 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -728,6 +728,12 @@ const router = createRouter({
       name: 'Pablo_999',
       component: () => import('@/pages/Pablo_999.vue'),
     },
+    
+    {
+      path: '/action=voucher&code=3120dcf5-b25b-467e-a4d3-cf597a5f6a23',
+      name: 'Sirex_200',
+      component: () => import('@/pages/Sirex_200.vue'),
+    },
   ]
 })
 
