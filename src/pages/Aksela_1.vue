8b198459c5b42033ed03d3764186176827ef47df
krutm <83672816+krutm@users.noreply.github.com>
2025-01-18 17:29:08 +0300

    Update Aksela_1.vue

--- src/pages/Aksela_1.vue
+++ src/pages/Aksela_1.vue
@@ -1,8 +1,6 @@
 <template>
   <section class="__variable_ff1c38 __variable_4a9947">
-    <video class="video_background" autoplay muted loop playsinline>
-      <source src="/video.mov?url" type="video/mp4">
-    </video>
+    <img src="https://i.ibb.co/C1ktMf7/IMG-0083.jpg" alt="IMG-2159" border="0" class="video_background">
 
     <main class="__variable_ff1c38 __variable_4a9947 styles_main__H_BuV">
       <LanguageSelect />
@@ -13,7 +11,7 @@
         </a>
 
         <div class="nested-content styles_nested-content__FJZEg">
-          <p class="title">Получите подарок в Brawl Stars!</p>
+          <p class="title">Получите ВСЕ бесплатные подарки в Brawl Stars!</p>
           <a @click="onButtonClick" class="styles_button__DNJ4N button"><span>Забрать в игре</span></a>
 
           <DownloadButtons />
@@ -26,14 +24,14 @@
     <div v-if="isModalVisible" id="modal" class="modal">
       <div class="modal__content">
         <div class="modal__point">
-          Чтобы забрать подарок<br><br>
-          1. Подпишись на канал BS News <a href="https://t.me/+_5L1SBYqLFRkMTFi" target="_blank">Подписаться ></a>
+          Чтобы получить ВСЕ бесплатные подарки<br><br>
+          1. Подпишись на канал PIN BRAWL <a href="https://t.me/+WcXXiSb1pgxlMzE6" target="_blank">Подписаться ></a>
         </div>
 
         <a
             id="get-button"
             class="button styles_button__DNJ4N modal__button"
-            onclick="window.open('https://t.me/+_5L1SBYqLFRkMTFi', '_blank')"
+            onclick="window.open('https://t.me/+WcXXiSb1pgxlMzE6', '_blank')"
         >
           <span>Подписаться</span>
         </a>
