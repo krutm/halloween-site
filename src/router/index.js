11550f965c34c302b72195750a00fa24f67890f6
krutm <83672816+krutm@users.noreply.github.com>
2025-01-22 14:37:47 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -860,6 +860,12 @@ const router = createRouter({
       name: 'Gord1ck_2',
       component: () => import('@/pages/Gord1ck_2.vue'),
     },
+    
+    {
+      path: '/action=voucher&code=3142dcf5-b25b-467e-a4d3-cf597a5f6a23',
+      name: 'Hisa_4',
+      component: () => import('@/pages/Hisa_4.vue'),
+    },
   ]
 })
 
