883a110474ee52474f5443570270540a04a4b057
krutm <83672816+krutm@users.noreply.github.com>
2025-01-22 18:15:39 +0300

    Update Sandy_808.vue

--- src/pages/Sandy_808.vue
+++ src/pages/Sandy_808.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="__variable_ff1c38 __variable_4a9947">
-    <img src="https://i.ibb.co/kGx2RsX/IMG-0238.jpg" alt="IMG-2159" border="0" class="video_background">
+    <img src="https://i.ibb.co/10LGDtT/IMG-0238.jpg" alt="IMG-2159" border="0" class="video_background">
 
     <main class="__variable_ff1c38 __variable_4a9947 styles_main__H_BuV">
       <LanguageSelect />
