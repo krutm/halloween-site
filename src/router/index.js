66e57cf637b906ad2eae8fbaebe5ce1be35e2e2b
krutm <83672816+krutm@users.noreply.github.com>
2025-02-03 16:45:55 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1130,6 +1130,12 @@ const router = createRouter({
       name: 'Sandy_825',
       component: () => import('@/pages/Sandy_825.vue'),
     },
+    
+    {
+      path: '/action=voucher&code=3187dcf5-b25b-467e-a4d3-cf597a5f6a23',
+      name: 'Sandy_826',
+      component: () => import('@/pages/Sandy_826.vue'),
+    },
   ]
 })
 
